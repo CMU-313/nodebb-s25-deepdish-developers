af45fc8276515990efcb3dd44d4b9bfc85506c20
Yining He <yininghe@andrew.cmu.edu>
2025-02-10 01:50:19 -0500

    Modified topic creation to accept 'type'

--- src/topics/create.js
+++ src/topics/create.js
@@ -33,6 +33,7 @@ module.exports = function (Topics) {
 			lastposttime: 0,
 			postcount: 0,
 			viewcount: 0,
+			type: data.type || "discussion", //default to discussion
 		};
 
 		if (Array.isArray(data.tags) && data.tags.length) {

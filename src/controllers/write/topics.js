ad30df18e37ab1c6729f78c96fa0e8b5a59bd838
ozcansid000 <sozcan@andrew.cmu.edu>
2025-02-25 13:31:14 -0500

    api controller function added to the incorrect file. Changed it from chats to topics per Jacob's code review

--- src/controllers/write/topics.js
+++ src/controllers/write/topics.js
@@ -207,3 +207,19 @@ Topics.bump = async (req, res) => {
 
 	helpers.formatApiResponse(200, res);
 };
+
+// Added controller functions to match API routes
+Chats.messages.markImportant = async (req, res) => {
+	const { mid, roomId } = req.params;
+	await api.chats.markMessageImportant(req, { mid, roomId });
+
+	helpers.formatApiResponse(200, res);
+};
+
+Chats.messages.unmarkImportant = async (req, res) => {
+	const { mid, roomId } = req.params;
+	await api.chats.unmarkMessageImportant(req, { mid, roomId });
+
+	helpers.formatApiResponse(200, res);
+};
+

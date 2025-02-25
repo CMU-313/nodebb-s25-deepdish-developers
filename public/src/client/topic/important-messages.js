2add5118704db19a9f3f06e8c977a6b5e976c4b5
ozcansid000 <sozcan@andrew.cmu.edu>
2025-02-25 16:46:02 -0500

    Cleaned up the file to simplify and correct chatgpt errors. Added useful comments

--- public/src/client/topic/important-messages.js
+++ public/src/client/topic/important-messages.js
@@ -1,29 +1,31 @@
+// Client file uses jQuery to change topics appearance by adding and removing the "important" CSS class
+// This file connects the front and backend.
+
 'use strict';
 
-define('forum/chats/important-messages', ['api', 'alerts'], function (api) {
+define('forum/topics/important-messages', ['api', 'alerts'], function (api) {
 	var importantMessages = {};
+
+    //marks a topic as important. 
+    // mid : message id
+    // tid : topic id
 	importantMessages.mark = function (mid, roomId) {
-		return api.put(`/chats/${roomId}/messages/${mid}/important`, {}).then(() => {
-			$(`[component="chat/message"][data-mid="${mid}"]`).addClass('important');
+		return api.put(`/topic/${tid}/messages/${mid}/important`, {}).then(() => {
+			$(`[component="topic/message"][data-mid="${mid}"]`).addClass('important');
 		});
 	};
 
+    //unmarks a topic as important. 
 	importantMessages.unmark = function (mid, roomId) {
-		return api.del(`/chats/${roomId}/messages/${mid}/important`, {})
+		return api.del(`/topic/${tid}/messages/${mid}/important`, {})
 			.then(() => {
-				$(`[component="chat/message"][data-mid="${mid}"]`).removeClass('important');
+				$(`[component="topic/message"][data-mid="${mid}"]`).removeClass('important');
 			});
 	};
 
+    //checks if already marked or unmarked and toggles it
 	importantMessages.toggle = function (mid, roomId) {
-		const messageElement = $(`[component="chat/message"][data-mid="${mid}"]`);
-		if (messageElement.hasClass('important')) {
-			return importantMessages.unmark(mid, roomId);
-		}
-		return importantMessages.mark(mid, roomId);
-	};
-	importantMessages.toggle = function (mid, roomId) {
-		const messageElement = $(`[component="chat/message"][data-mid="${mid}"]`);
+		const messageElement = $(`[component="topic/message"][data-mid="${mid}"]`);
 		if (messageElement.hasClass('important')) {
 			return importantMessages.unmark(mid, roomId);
 		}
@@ -32,18 +34,4 @@ define('forum/chats/important-messages', ['api', 'alerts'], function (api) {
 
 	return importantMessages;
 });
-
-
-// importantMessages.mark = function (mid, roomId) {
-//     api.put(`/chats/${roomId}/messages/${mid}/important`, {}).then(() => {
-//         $(`[component="chat/message"][data-mid="${mid}"]`).toggleClass('important', true);
-//     }).catch(alerts.error);
-// };
-
-// importantMessages.unmark = function (mid, roomId) {
-//     api.del(`/chats/${roomId}/messages/${mid}/important`, {}).then(() => {
-//         $(`[component="chat/message"][data-mid="${mid}"]`).toggleClass('important', false);
-//     }).catch(alerts.error);
-// };
-
 
